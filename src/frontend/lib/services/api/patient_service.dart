7f401c74e2d789302a853b36175aeed95fcf5369
Lobaali <161109796+Lobaali@users.noreply.github.com>
2026-02-16 11:18:03 +0300

    Database setup (#20)

--- src/frontend/lib/services/api/patient_service.dart
+++ src/frontend/lib/services/api/patient_service.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
-import '../../../config/api_config.dart';
+import '../../config/api_config.dart';
 import '../../dto/patient_dto.dart';
 
 class PatientService {
